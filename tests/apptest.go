aaf9aec5d50257ff627b8a7ffa90e4502669a490
Javier Aguirre <contacto@javaguirre.net>
2013-07-25 22:33:14 +0200

    First post test

--- tests/apptest.go
+++ tests/apptest.go
@@ -1,6 +1,12 @@
 package tests
 
-import "github.com/robfig/revel"
+import (
+    "github.com/robfig/revel"
+    "dashpersonal/app/models"
+    "encoding/json"
+    "log"
+    "bytes"
+)
 
 type AppTest struct {
 	revel.TestSuite
@@ -18,4 +24,26 @@ func (t AppTest) TestThatIndexPageWorks() {
 
 func (t AppTest) After() {
 	println("Tear down")
+}
+
+func (t AppTest) TestAddWidget() {
+    widget := models.Widget{"X", "1", "1", "1", "1", "Hello world"}
+    var widget_created models.Widget
+
+    content, err := json.Marshal(widget);
+
+    if err != nil {
+        log.Fatal("Error")
+    }
+
+    t.Post("/", "application/json", bytes.NewBuffer(content))
+    t.AssertOk()
+    t.AssertContentType("application/json")
+    err_created := json.Unmarshal(t.ResponseBody, &widget_created)
+
+    if err_created != nil {
+        log.Fatal("Error Response")
+    }
+
+    t.Assert(widget_created.Content == widget.Content)
 }
